d284ed19579a07020d26539cf25674f1f9da47cb
Juan Carlos <juanc6v@gmail.com>
2022-10-21 01:48:21 -0300

    rutas privadas

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,46 +1,59 @@
 import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom'
+import { useAuth } from '../context/auth';
 import Login from './Login';
 
 const routes = [];
 routes.push({
     id: 1,
     to: '/',
-    text: 'Home'
+    text: 'Home',
+    private: false
 });
 routes.push({
     id: 2,
     to: '/blog',
-    text: 'Notas'
+    text: 'Notas',
+    private: false
 });
+routes.push({
+    id: 3,
+    to: '/profile',
+    text: 'Perfil',
+    private: true
+});
+// routes.push({
+//     id: 4,
+//     to: '/login',
+//     text: 'Login',
+//     publicOnly: true
+// });
+
 
 
 const Main = () => {
-    const [login, setLogin] = useState(false)
-    const showLogin = () => {
-        setLogin(!login)
-    }
+
+    const auth = useAuth();
 
     return (
         <nav>
             <ul>
-                {routes.map(route => (
-                    <li key={route.to}>
-                        <NavLink style={({ isActive }) => ({
-                            color: isActive ? 'red' : 'blue',
-                        })}
-                            to={route.to} end>
-                            {route.text}
-                        </NavLink>
-                    </li>
-                ))}
+                {routes.map(route => {
+                    if (route.private && !auth.user) return null
+                    return (
+                        <li key={route.to}>
+                            <NavLink style={({ isActive }) => ({
+                                color: isActive ? 'red' : 'blue',
+                            })}
+                                to={route.to} end>
+                                {route.text}
+                            </NavLink>
+                        </li>)
+                })}
             </ul>
 
             <Login />
-            {/* {
-                showLogin? <Login />
-                
-            } */}
+
         </nav>
     )
 }
